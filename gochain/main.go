620d94770c2633e7e24d3dbbc6a8c961563c39ff
fenchelfen <m.lyamets@innopolis.ru>
2020-10-25 14:58:39 +0300

    Start blockchain

--- gochain/main.go
+++ gochain/main.go
@@ -10,12 +10,29 @@ type block struct {
 	prevBlockHash string
 }
 
+type blockchain struct {
+	blocks []*block
+}
+
 func CreateBlock(index int64, transactions []*models.Transaction, prevBlockHash string) *block {
 
 	return &block{index, transactions, prevBlockHash}
 }
 
-func (b *block) computeHashSum() string {
+func CreateBlockChain() *blockchain {
+
+	chain := []*block{
+		CreateBlock(0, nil, ""),
+	}
+	return &blockchain{chain}
+}
+
+func (b *block) ComputeHashSum() string {
 
 	return ""
+}
+
+func (c *blockchain) LastBlock() *block {
+
+	return c.blocks[len(c.blocks)-1]
 }
