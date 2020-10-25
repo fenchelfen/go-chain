5b6b922d39da3e2c85fd56eafcbb4c47b13785bd
fenchelfen <m.lyamets@innopolis.ru>
2020-10-25 15:27:28 +0300

    Get digest and prove work

--- gochain/main.go
+++ gochain/main.go
@@ -1,13 +1,17 @@
 package gochain
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"gochain/models"
+	"strings"
 )
 
 type block struct {
 	index         int64
 	transactions  []*models.Transaction
 	prevBlockHash string
+	nonce         int64
 }
 
 type blockchain struct {
@@ -16,13 +20,16 @@ type blockchain struct {
 
 func CreateBlock(index int64, transactions []*models.Transaction, prevBlockHash string) *block {
 
-	return &block{index, transactions, prevBlockHash}
+	return &block{index, transactions, prevBlockHash, 0}
 }
 
 func CreateBlockChain() *blockchain {
 
+	genesisBlock := CreateBlock(0, nil, "")
+	genesisBlock.prevBlockHash = genesisBlock.ComputeHashSum()
+
 	chain := []*block{
-		CreateBlock(0, nil, ""),
+		genesisBlock,
 	}
 	return &blockchain{chain}
 }
@@ -32,7 +39,34 @@ func (b *block) ComputeHashSum() string {
 	return ""
 }
 
+func (b *block) getDigest(nonce int) string {
+
+	var hash = ""
+
+	for i := 0; i < len(b.transactions); i++ {
+
+		hash = fmt.Sprintf(
+			"%x",
+			sha256.Sum256([]byte(hash)),
+		)
+	}
+
+	return hash
+}
+
 func (c *blockchain) LastBlock() *block {
 
 	return c.blocks[len(c.blocks)-1]
+}
+
+func (c *blockchain) ProveWork(b *block) string {
+
+	var nonce = 0
+
+	for strings.HasPrefix(b.getDigest(nonce), "00") {
+
+		nonce++
+	}
+
+	return ""
 }
